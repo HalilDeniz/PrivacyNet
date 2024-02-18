3e1f1726e984b2016bf2b58b325b2da9d14ed5ef
halil ibrahim deniz <62566319+HalilDeniz@users.noreply.github.com>
2024-02-18 10:57:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'privacynet=privacynet.privacynet:privacynet',
+            'privacynet=privacynet.privacynet:main',
         ],
     },
     classifiers=[
